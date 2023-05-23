2b47a469f46b8000bcebdc19d4692649c436df68
Crowdbotics <team@crowdbotics.com>
2023-05-23 00:07:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "eye-deal-40908",
+  url: "https://eye-deal-40908.botics.co",
+  api: "https://eye-deal-40908.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
